2d4938631a23a69e087ebf67d8d0fe142d257c10
neketta300 <neketta300@gmail.com>
2024-11-11 23:26:10 +0300

    added login & register pages

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,166 @@
+import 'package:del_app_green/components/my_icon_button.dart';
+import 'package:del_app_green/components/my_primary_button.dart';
+import 'package:del_app_green/components/my_textfield.dart';
+import 'package:del_app_green/pages/main_screen.dart';
+import 'package:del_app_green/pages/register_page.dart';
+import 'package:del_app_green/themes/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailOrPhoneTextController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 85,
+            ),
+            Center(
+              child: SvgPicture.asset(
+                "assets/images/logo_login_page.svg",
+                height: 100,
+              ),
+            ),
+            Text(
+              "Waves Of Food",
+              style: GoogleFonts.yeonSung(color: Colors.black, fontSize: 40),
+            ),
+            Text(
+              "Deliver Favorite Food",
+              style: GoogleFonts.lato(fontSize: 14, color: mainColorText),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Login To Your  Account",
+              style: GoogleFonts.lato(
+                  fontSize: 20,
+                  color: mainColorText,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            MyTextfield(
+              hintText: 'Email of Phone Number',
+              textEditingController: emailOrPhoneTextController,
+              obscureText: false,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            MyTextfield(
+              hintText: 'Password',
+              textEditingController: passwordController,
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Or",
+              style: GoogleFonts.yeonSung(
+                  fontSize: 14, color: const Color.fromRGBO(9, 5, 28, 1)),
+            ),
+            Text(
+              "Continue With",
+              style: GoogleFonts.yeonSung(
+                fontSize: 20,
+                color: const Color.fromRGBO(9, 5, 28, 1),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyIconButton(
+                  imagePath: "assets/images/communication.png",
+                  text: 'Facebook',
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                MyIconButton(
+                  imagePath: "assets/images/google.png",
+                  text: 'Google',
+                  onTap: () {},
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MyPrimaryButton(
+              text: 'Login',
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MainScreen(),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const RegisterPage(),
+                ),
+              ),
+              child: GradientText(
+                "Don’t Have Account?",
+                style: GoogleFonts.lato(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 10,
+                ),
+                colors: const [
+                  Color.fromRGBO(83, 232, 139, 1),
+                  Color.fromRGBO(21, 190, 119, 1),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GradientText(
+              textAlign: TextAlign.center,
+              "Made by\nN3H",
+              style: GoogleFonts.yeonSung(
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+              ),
+              colors: const [
+                Color.fromRGBO(83, 232, 139, 1),
+                Color.fromRGBO(21, 190, 119, 1),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,151 @@
+import 'package:del_app_green/components/my_icon_button.dart';
+import 'package:del_app_green/components/my_primary_button.dart';
+import 'package:del_app_green/components/my_textfield.dart';
+import 'package:del_app_green/themes/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController emailOrPhoneTextController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 85,
+            ),
+            Center(
+              child: SvgPicture.asset(
+                "assets/images/logo_login_page.svg",
+                height: 100,
+              ),
+            ),
+            Text(
+              "Waves Of Food",
+              style: GoogleFonts.yeonSung(color: Colors.black, fontSize: 40),
+            ),
+            Text(
+              "Deliver Favorite Food",
+              style: GoogleFonts.lato(
+                  fontSize: 14,
+                  color: mainColorText,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Sign Up Here",
+              style: GoogleFonts.lato(
+                  fontSize: 20,
+                  color: mainColorText,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            MyTextfield(
+              hintText: 'Name',
+              textEditingController: nameController,
+              obscureText: false,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            MyTextfield(
+              hintText: 'Email of Phone Number',
+              textEditingController: emailOrPhoneTextController,
+              obscureText: false,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            MyTextfield(
+              hintText: 'Password',
+              textEditingController: passwordController,
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Or",
+              style: GoogleFonts.yeonSung(
+                  fontSize: 14, color: const Color.fromRGBO(9, 5, 28, 1)),
+            ),
+            Text(
+              "Sign Up With",
+              style: GoogleFonts.yeonSung(
+                fontSize: 20,
+                color: const Color.fromRGBO(9, 5, 28, 1),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyIconButton(
+                  imagePath: "assets/images/communication.png",
+                  text: 'Facebook',
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                MyIconButton(
+                  imagePath: "assets/images/google.png",
+                  text: 'Google',
+                  onTap: () {},
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MyPrimaryButton(
+              text: 'Create account',
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: GradientText(
+                "Already Have An Account?",
+                style: GoogleFonts.lato(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 10,
+                ),
+                colors: const [
+                  Color.fromRGBO(83, 232, 139, 1),
+                  Color.fromRGBO(21, 190, 119, 1),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
