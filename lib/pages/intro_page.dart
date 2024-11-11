4096318a566653517588e4b6b55b9470ce2eaf16
neketta300 <neketta300@gmail.com>
2024-11-11 21:24:40 +0300

    added intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,10 +1,61 @@
+import 'package:del_app_green/components/my_primary_button.dart';
+import 'package:del_app_green/pages/login_page.dart';
+import 'package:del_app_green/themes/fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Center(
+              child: SvgPicture.asset(
+                "assets/images/intro_page.svg",
+                height: 312,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Enjoy Restaurant Quality Meals\nat Home",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.yeonSung(color: mainColorText, fontSize: 20),
+            ),
+            const SizedBox(
+              height: 220,
+            ),
+            MyPrimaryButton(
+              text: "Next",
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Made by N3H",
+              style: GoogleFonts.yeonSung(color: mainColorText, fontSize: 16),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
