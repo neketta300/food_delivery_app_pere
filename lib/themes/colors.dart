2c4cb6b5463dee65489a557ec0ec569ff29b0f0f
neketta300 <neketta300@gmail.com>
2024-11-14 20:58:27 +0300

    add new colors & gradients

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,3 +1,15 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 Color mainColorText = const Color.fromARGB(255, 83, 232, 140);
+LinearGradient myGradient = const LinearGradient(
+  colors: [
+    Color.fromARGB(255, 83, 232, 139),
+    Color.fromARGB(255, 21, 190, 119)
+  ],
+);
+List<Color> myTextGradient = const <Color>[
+  Color.fromARGB(255, 83, 232, 139),
+  Color.fromARGB(255, 21, 190, 119)
+];
