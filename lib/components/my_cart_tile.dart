b05de46d36306fb7a6092ff9f316a4552d07cebc
neketta300 <neketta300@gmail.com>
2024-11-14 20:57:37 +0300

    added cart tile

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -1,17 +1,23 @@
+import 'package:del_app_green/components/my_quantity_selector.dart';
+import 'package:del_app_green/main.dart';
 import 'package:del_app_green/models/cart_item.dart';
+import 'package:del_app_green/models/shop.dart';
+import 'package:del_app_green/themes/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class MyCartTile extends StatelessWidget {
-  final CartItem item;
+  final CartItem cartItem;
   const MyCartTile({
     super.key,
-    required this.item,
+    required this.cartItem,
   });
 
   @override
   Widget build(BuildContext context) {
+    final shop = context.read<Shop>();
     return GestureDetector(
       child: Container(
         margin: const EdgeInsets.only(left: 20, right: 20, bottom: 25),
@@ -25,7 +31,7 @@ class MyCartTile extends StatelessWidget {
             ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Image.asset(
-                item.food.imagePath,
+                cartItem.food.imagePath,
                 fit: BoxFit.cover,
                 height: 74,
                 width: 74,
@@ -34,20 +40,33 @@ class MyCartTile extends StatelessWidget {
             const SizedBox(
               width: 20,
             ),
-            Text(
-              item.food.name,
-              style: GoogleFonts.yeonSung(fontSize: 18),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  cartItem.food.name,
+                  style: GoogleFonts.yeonSung(fontSize: 18),
+                ),
+                GradientText(
+                  colors: myTextGradient,
+                  "\$" + cartItem.food.price.toString(),
+                  style: GoogleFonts.lato(
+                      fontSize: 22, fontWeight: FontWeight.w900),
+                ),
+              ],
             ),
             const Spacer(),
-            GradientText(
-              colors: const [
-                Color.fromRGBO(83, 232, 139, 1),
-                Color.fromRGBO(21, 190, 119, 1),
-              ],
-              "\$" + item.food.price.toString(),
-              style:
-                  GoogleFonts.lato(fontSize: 22, fontWeight: FontWeight.w900),
-            ),
+            MyQuantitySelector(
+                food: cartItem.food,
+                quantity: cartItem.quntity,
+                onIncrement: () {
+                  shop.addToCart(cartItem.food);
+                },
+                onDecrement: () {
+                  if (cartItem.quntity > 1) {
+                    shop.removeFromCart(cartItem);
+                  } else if (cartItem.quntity == 1) {}
+                }),
             const SizedBox(
               width: 10,
             ),
